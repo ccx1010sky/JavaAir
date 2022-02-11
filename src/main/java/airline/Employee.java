66cdcc0dfdf09b1deae672f9a787d329fda78412
ccx1010sky <ccx1010@gmail.com>
2022-02-11 15:30:07 +0000

    set up Employee abstract class constructor,name and crewRank

--- src/main/java/airline/Employee.java
+++ src/main/java/airline/Employee.java
@@ -1,5 +1,11 @@
 package airline;
 
 public abstract class Employee {
+    String name;
+    CrewRank crewRank;
+    public void Employee(String name,CrewRank crewRank){
+        this.name = name;
+        this.crewRank = crewRank;
+    }
 
 }
