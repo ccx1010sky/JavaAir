96c3dda775805e408a5ae57856f75b2bd9822065
ccx1010sky <ccx1010@gmail.com>
2022-02-11 22:19:19 +0000

    setup passenger class,name,numberOfBages

---
+++ src/main/java/passenger/Passenger.java
@@ -0,0 +1,10 @@
+package passenger;
+
+public class Passenger {
+    private String name;
+    private int numberOfBags;
+    public  Passenger(String name,int numberOfBags){
+        this.name = name;
+        this.numberOfBags = numberOfBags;
+    }
+}
