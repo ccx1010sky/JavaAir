f3663d40d3ac7d0e905261a03ac6834f52a44934
ccx1010sky <ccx1010@gmail.com>
2022-02-11 22:32:10 +0000

    declare pilot ccmembers,passengers

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -8,6 +8,23 @@ public class Flight {
     private Pilot pilot;
     ArrayList<CcMember> ccMembers;
     ArrayList<Passenger> passengers;
+    private PlaneType planeType;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight( Pilot pilot,PlaneType planeType,String flightNumber,String destination,String departureAirport,String departureTime){
+        this.pilot = pilot;
+        this.planeType = planeType;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+        ccMembers = new ArrayList<>(7);
+        passengers = new ArrayList<>(200);
+    }
+
 
 
 }
