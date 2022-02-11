05b8fa050524292e9ac4ac336a9d6223f670b475
ccx1010sky <ccx1010@gmail.com>
2022-02-11 22:32:10 +0000

    declare pilot ccmembers,passengers

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -1,4 +1,13 @@
 package airline;
 
+import passenger.Passenger;
+
+import java.util.ArrayList;
+
 public class Flight {
+    private Pilot pilot;
+    ArrayList<CcMember> ccMembers;
+    ArrayList<Passenger> passengers;
+
+
 }
