06624a39d6e0c23c083b915b332d33c5ed63b669
ccx1010sky <ccx1010@gmail.com>
2022-02-13 19:09:26 +0000

    create GLA_EDI and EDI_LHR info

--- src/main/java/airline/FlightInfo.java
+++ src/main/java/airline/FlightInfo.java
@@ -1,4 +1,14 @@
 package airline;
 
 public enum FlightInfo {
+    GLA_EDI("FR756","GLA","EDI","8:00 am"),
+    EDI_LHR("EL3456","EDI","LHR","9:00am");
+
+    String fightNumber;
+    String departureAirport;
+    String destination;
+    String time;
+    FlightInfo(String flightNumber,String departureAirport,String destination, String time){
+
+    }
 }
