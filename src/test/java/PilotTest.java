53a06ec6c5f38e6a262df61f720c0a6eaa1af4f4
ccx1010sky <ccx1010@gmail.com>
2022-02-13 20:11:22 +0000

    pilot haName test passed

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,11 +1,19 @@
 import airline.CrewRank;
 import airline.Pilot;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PilotTest {
     Pilot pilot;
     @Before
     public void pilot(){
         pilot = new Pilot("Mike", CrewRank.CAPTAIN,"CCA0001");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Mike",pilot.getName());
     }
 }
