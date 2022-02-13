2852136aaf45d2fce26c1dcc0776159e3797dae8
ccx1010sky <ccx1010@gmail.com>
2022-02-13 20:16:41 +0000

    @Before

--- src/test/java/CcMemberTest.java
+++ src/test/java/CcMemberTest.java
@@ -1,2 +1,15 @@
+import airline.CcMember;
+import airline.CrewRank;
+import org.junit.Before;
+
 public class CcMemberTest {
+        private CcMember ccMember;
+
+        @Before
+        public void before(){
+            ccMember = new CcMember("John", CrewRank.FIRST_OFFICER);
+        }
+
+
+
 }
