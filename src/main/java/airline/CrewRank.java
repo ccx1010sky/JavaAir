415be06ca600641f6e2586095118a3acf0761348
ccx1010sky <ccx1010@gmail.com>
2022-02-13 18:56:16 +0000

    setup enum CrewRank

--- src/main/java/airline/CrewRank.java
+++ src/main/java/airline/CrewRank.java
@@ -1,9 +1,16 @@
 package airline;
 
 public enum CrewRank {
-    CAPTAIN,
-    FIRST_OFFICER,
-    PURSER,
-    FLIGHT_ATTENDANT,
-    FLIGHT_MANAGER,
+    CAPTAIN(1,1),
+    FIRST_OFFICER(2,1),
+    PURSER(3,4),
+    FLIGHT_ATTENDANT(4,6);
+    private int rank;
+    private int numberOfCrew;
+    CrewRank(int rank,int numberOfCrew){
+        this.rank = rank;
+        this.numberOfCrew = numberOfCrew;
+    }
+
+    }
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,11 @@
+import airline.CrewRank;
+import airline.Pilot;
+import org.junit.Before;
+
 public class PilotTest {
+    Pilot pilot;
+    @Before
+    public void pilot(){
+        pilot = new Pilot("Mike", CrewRank.CAPTAIN,"CCA0001");
+    }
 }
