4e6f7292c1c9c4f1021d58481f7076625becf8c5
ccx1010sky <ccx1010@gmail.com>
2022-02-13 19:17:31 +0000

    modify flightInfo

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -11,19 +11,21 @@ public class Flight {
     ArrayList<Pilot> pilots;
     ArrayList<CcMember> ccMembers;
     ArrayList<Passenger> passengers;
+    private FlightInfo flightInfo;
     private PlaneType planeType;
-    private String flightNumber;
-    private String destination;
-    private String departureAirport;
-    private String departureTime;
+//    private String flightNumber;
+//    private String destination;
+//    private String departureAirport;
+//    private String departureTime;
 
-    public Flight( Pilot pilot,PlaneType planeType,String flightNumber,String destination,String departureAirport,String departureTime){
+    public Flight( Pilot pilot,PlaneType planeType,FlightInfo flightInfo){
         this.pilot = pilot;
         this.planeType = planeType;
-        this.flightNumber = flightNumber;
-        this.destination = destination;
-        this.departureAirport = departureAirport;
-        this.departureTime = departureTime;
+        this.flightInfo = flightInfo;
+//        this.flightNumber = flightNumber;
+//        this.destination = destination;
+//        this.departureAirport = departureAirport;
+//        this.departureTime = departureTime;
         pilots = new ArrayList<>();
         ccMembers = new ArrayList<>();
         passengers = new ArrayList<>();
