8f08ea7eca1ef8f80de60a7e0811a901425693f0
ccx1010sky <ccx1010@gmail.com>
2022-02-13 16:51:58 +0000

    assertEquals()

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import passenger.Passenger;
 
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
 
     private Passenger passenger1;
@@ -8,9 +10,11 @@ public class PassengerTest {
     @Before
     public void before(){
         passenger1= new Passenger("Joe",3);
-
     }
 
+    public void hasName(){
+        assertEquals("Joe",passenger1.getName());
+    }
 
 
 
